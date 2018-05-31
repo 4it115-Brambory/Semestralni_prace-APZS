1fd7850874a3b5a264c6ff553a5a6d210177b7c1
Admiral Pike <Admiral Pike@10.0.0.32>
2018-05-31 19:26:09 +0200

    Oprava odhlášení a opětovného přihlášení

--- src/main/java/com/github/it115_Brambory/Semestralni_prace_APZS/ui/PrehledakcibuddyController.java
+++ src/main/java/com/github/it115_Brambory/Semestralni_prace_APZS/ui/PrehledakcibuddyController.java
@@ -1,75 +1,102 @@
 package com.github.it115_Brambory.Semestralni_prace_APZS.ui;
 
 import com.github.it115_Brambory.Semestralni_prace_APZS.logika.*;
-import com.github.it115_Brambory.Semestralni_prace_APZS.main.Start;
-
 import java.util.Observer;
-import java.awt.TextField;
+
+import javax.persistence.Table;
+import javafx.event.ActionEvent;
 import java.sql.SQLException;
 import java.util.Observable;
-
-import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
- * Kontroler, který zprostředkovává komunikaci mezi grafikou
- * a logikou - slouží pro načtení přehledu buddy
+ * Kontroler, který zprostředkovává komunikaci mezi grafikou a logikou - slouží
+ * pro načtení přehledu buddy
  * 
  * @author Jan Mandík
  *
  */
 public class PrehledakcibuddyController extends Pane implements Observer {
 
-		//zjistit, jak se používá tableview
-		private IBuddyAplikace buddyAplikace;	
-		@FXML private TableColumn typ;
-		@FXML private TableColumn nazev;
-		@FXML private TableColumn casDo;
-		@FXML private TableColumn casOd;
-		@FXML private TableColumn misto;
-
-		@FXML private TableColumn cena;
-		@FXML private TextArea prihlasen;
+	// zjistit, jak se používá tableview
+	private IBuddyAplikace buddyAplikace;
+	@FXML
+	private Pane thisPane;
+	@FXML
+	private Button odhlasit;
+	@FXML
+	private TableView<Table> tableView;
+	@FXML
+	private TableColumn<Table, String> typ;
+	@FXML
+	private TableColumn<Table, String> nazev;
+	@FXML
+	private TableColumn<Table, String> casOd;
+	@FXML
+	private TableColumn<Table, String> casDo;
+	@FXML
+	private TableColumn<Table, String> misto;
+	@FXML
+	private TableColumn<Table, Integer> cena;
+	@FXML
+	private TextArea prihlasen;
 
 	/**
-     *  Metoda k inicializaci hry. Načte všechny potřebné prvky
-     *  GUI a přidá observery.
-     *  
-	 * @throws SQLException - to je kvůli těm testům na konci metody
-     */
+	 * Metoda k inicializaci hry. Načte všechny potřebné prvky GUI a přidá
+	 * observery.
+	 * 
+	 * @throws SQLException
+	 *             - to je kvůli těm testům na konci metody
+	 */
 	public void inicializuj(IBuddyAplikace buddyAplikace) throws SQLException {
-		
 		this.buddyAplikace = buddyAplikace;
 		prihlasen.setText(buddyAplikace.getBuddyAplikace().getAktualniUzivatel().getEmail());
 		prihlasen.setEditable(false);
+
+		//nacti data do tabulky
+		typ.setCellValueFactory(new PropertyValueFactory("firstName"));
 		
 		
-		
-		
-		//---------------------------------------------------------------------------------------------------------------------------------
-		//Ahoj, tohle mi tady nechte zakomentovaný, jsou to testy, že funguje databázový připojení
-		//a že se data správně vkládaj do programu
-		
-		//jmeno.setText(this); Potřeba zíkávat hodnotu jednotlivých fieldů, nevím, jak to udělat
-		//this.buddyAplikace.getBuddyAplikace().getDatabazeOperace().logInTest();
 		
 	}
-	
+
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
 	}
-	
-	//odhlašovací metoda, hádám, že bude stačit zabít celou aplikaci
-		@FXML
-		private void odhlasit() throws Exception {
-			Platform.exit();
-			Start.main(null); //tohle by mělo aplikaci zas spustit, ale nějak se to neděje, tak na to kašlu. Hlavně, že se to zavře
-		}
-	
-}
 
+	// odhlašovací metoda, hádám, že bude stačit zabít celou aplikaci
+	// -------- a nebo, musi se nastavit aktualniUzivatel v buddyAplikaci na null
+	// -------- a vratit se na prihlasovaci okno. Zkusim tuhle cestu, protoze zatim
+	// -------- vam zmena okna nikde nefunguje, tak at to pak zkopirujete
+	@FXML
+	public void odhlasit(ActionEvent event) throws Exception {
+		
+		this.buddyAplikace.getBuddyAplikace().logOut();
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(this.getClass().getResource("prihlaseni.fxml"));
+		Parent tableViewParent = loader.load();
+    	
+		Scene tableViewScene = new Scene(tableViewParent);
+		
+		PrihlaseniController controller = loader.getController();
+		controller.inicializuj(buddyAplikace);
+		
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(tableViewScene);
+		window.show();
+		
+	}
+
+}
--- src/main/java/com/github/it115_Brambory/Semestralni_prace_APZS/ui/PrihlaseniController.java
+++ src/main/java/com/github/it115_Brambory/Semestralni_prace_APZS/ui/PrihlaseniController.java
@@ -12,6 +12,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -35,20 +36,14 @@ import javafx.scene.control.TextField;
 public class PrihlaseniController extends Pane implements Observer {
 
 	private IBuddyAplikace buddyAplikace;
-	private Parent root;
-	private Stage window;
-
 	@FXML
 	private TextField email;
 	@FXML
 	private PasswordField heslo;
 
 	/**
-	 * Metoda k inicializaci hry. Načte všechny potřebné prvky GUI a přidá
-	 * observery.
+	 * Metoda k inicializaci hry.
 	 * 
-	 * @throws SQLException
-	 *             - to je kvůli těm testům na konci metody
 	 */
 	public void inicializuj(IBuddyAplikace buddyAplikace) throws SQLException {
 
@@ -61,18 +56,19 @@ public class PrihlaseniController extends Pane implements Observer {
 	 * Tahle metoda by se měla postarat o změnu Stage, nicméně nefunguje a nevím
 	 * proč. Myslím, že by ještě měla načítat kontroler
 	 */
-	private void sceneExchange () throws Exception {
+	private void sceneExchange (ActionEvent event) throws Exception {
 		FXMLLoader loader = new FXMLLoader();
-    	loader.setLocation(getClass().getResource("Prehledakciexchange.fxml"));    	
-    	Parent root = loader.load();
-    	PrehledakciexchangeController controller = new PrehledakciexchangeController();
-    	controller = loader.getController(); 
-    	controller.inicializuj(buddyAplikace);
-    	Stage Prehledakciexchange = new Stage();    	
-    	Prehledakciexchange.setScene(new Scene(root));
-    	Prehledakciexchange.show();
-    	Prehledakciexchange.setTitle("Přehled akcí");
+		loader.setLocation(this.getClass().getResource("Prehledakciexchange.fxml"));
+		Parent tableViewParent = loader.load();
     	
+		Scene tableViewScene = new Scene(tableViewParent);
+		
+		PrehledakcibuddyController controller = loader.getController();
+		controller.inicializuj(buddyAplikace);
+		
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(tableViewScene);
+		window.show();
 	}
 
 	/**
@@ -80,18 +76,19 @@ public class PrihlaseniController extends Pane implements Observer {
 	 * proč. Myslím, že by ještě měla načítat kontroler
 	 */
 	@FXML
-	private void sceneAdmin() throws Exception {
+	private void sceneAdmin(ActionEvent event) throws Exception {
 		FXMLLoader loader = new FXMLLoader();
-    	loader.setLocation(getClass().getResource("Prehledakciadmin.fxml"));    	
-    	Parent root = loader.load();
-    	PrehledakciadminController controller = new PrehledakciadminController();
-    	controller = loader.getController(); 
-    	controller.inicializuj(buddyAplikace);
-    	Stage Prehledakciadmin = new Stage();    	
-    	Prehledakciadmin.setScene(new Scene(root));
-    	Prehledakciadmin.show();
-    	Prehledakciadmin.setTitle("Přehled akcí");
+		loader.setLocation(this.getClass().getResource("Prehledakciadmin.fxml"));
+		Parent tableViewParent = loader.load();
     	
+		Scene tableViewScene = new Scene(tableViewParent);
+		
+		PrehledakcibuddyController controller = loader.getController();
+		controller.inicializuj(buddyAplikace);
+		
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(tableViewScene);
+		window.show();
 	}
 
 	/**
@@ -99,22 +96,23 @@ public class PrihlaseniController extends Pane implements Observer {
 	 * proč. Myslím, že by ještě měla načítat kontroler
 	 */
 	@FXML
-	private void sceneBuddy() throws Exception {
+	private void sceneBuddy(ActionEvent event) throws Exception {
 		FXMLLoader loader = new FXMLLoader();
-    	loader.setLocation(getClass().getResource("Prehledakcibuddy.fxml"));    	
-    	Parent root = loader.load();
-    	PrehledakcibuddyController controller = new PrehledakcibuddyController();
-    	controller = loader.getController(); 
-    	controller.inicializuj(buddyAplikace);
-    	Stage Prehledakcibuddy = new Stage();    	
-    	Prehledakcibuddy.setScene(new Scene(root));
-    	Prehledakcibuddy.show();
-    	Prehledakcibuddy.setTitle("Přehled akcí");
+		loader.setLocation(this.getClass().getResource("Prehledakcibuddy.fxml"));
+		Parent tableViewParent = loader.load();
     	
+		Scene tableViewScene = new Scene(tableViewParent);
+		
+		PrehledakcibuddyController controller = loader.getController();
+		controller.inicializuj(buddyAplikace);
+		
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(tableViewScene);
+		window.show();
 	}
 
 	@FXML
-	public void prihlasit() throws Exception {
+	public void prihlasit(ActionEvent event) throws Exception {
 
 		//kontrolní výpis emailu a hesla
 		System.out.println("Kontrolní výpis emailu -->" + email.getText() + "<--");
@@ -126,19 +124,19 @@ public class PrihlaseniController extends Pane implements Observer {
 			//vyskočí modální okno
 			ukazAlertBad();
 		} else if (ukazatel == 1) {
-			Stage curretStage = (Stage) email.getScene().getWindow();
-			sceneBuddy();  /* přepnutí na buddyho */
-			curretStage.close(); 
+			//Stage curretStage = (Stage) email.getScene().getWindow();
+			sceneBuddy(event);  /* přepnutí na buddyho */
+			//curretStage.close(); 
 			
 		} else if (ukazatel == 2) {
-			Stage curretStage = (Stage) email.getScene().getWindow();
-			sceneExchange(); /* přepnutí na exchange */
-			curretStage.close();
+			//Stage curretStage = (Stage) email.getScene().getWindow();
+			sceneExchange(event); /* přepnutí na exchange */
+			//curretStage.close();
 			;
 		} else {
-			Stage curretStage = (Stage) email.getScene().getWindow();
-			sceneAdmin();   /* přepnutí na admina */;
-			curretStage.close();
+			//Stage curretStage = (Stage) email.getScene().getWindow();
+			sceneAdmin(event);   /* přepnutí na admina */;
+			//curretStage.close();
 		}
 	}
 
@@ -158,11 +156,4 @@ public class PrihlaseniController extends Pane implements Observer {
 		// TODO Auto-generated method stub
 	}
 	
-	//odhlašovací metoda, hádám, že bude stačit zabít celou aplikaci
-		@FXML
-		private void odhlasit() throws Exception {
-			Platform.exit();
-			Start.main(null); //tohle by mělo aplikaci zas spustit, ale nějak se to neděje, tak na to kašlu. Hlavně, že se to zavře
-		}
-
 }
