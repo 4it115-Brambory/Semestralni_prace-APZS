93da153ace2a08addbad268539ee39219a766b3e
Admiral Pike <Admiral Pike@10.0.0.32>
2018-06-01 14:19:39 +0200

    Kontrola plnosti akce u schvalování žádosti - admin

--- src/main/java/com/github/it115_Brambory/Semestralni_prace_APZS/ui/PrehledZadostiController.java
+++ src/main/java/com/github/it115_Brambory/Semestralni_prace_APZS/ui/PrehledZadostiController.java
@@ -110,7 +110,7 @@ public class PrehledZadostiController extends Pane implements Observer {
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		
+
 	}
 
 	@FXML
@@ -213,8 +213,16 @@ public class PrehledZadostiController extends Pane implements Observer {
 		// schvalit jen to co u ceho je ---
 		if (selectedRequest != null) {
 			if (selectedRequest.getSchvaleno() == "---") {
-				this.buddyAplikace.getBuddyAplikace().getDatabazeOperace().setRequestSchvaleno(selectedRequest.getId());
-				scenePrehledZadosti(event);
+				if (this.buddyAplikace.getBuddyAplikace().getDatabazeOperace()
+						.zjistiObsazenaAkceProExchange(this.buddyAplikace.getBuddyAplikace().getDatabazeOperace()
+								.zjistiAkceIdPodleRequestId(selectedRequest.getId()))) {
+
+					ukazAlertGood("Akce je již plná, nezle schvalovat další studenty.");
+				} else {
+					this.buddyAplikace.getBuddyAplikace().getDatabazeOperace()
+							.setRequestSchvaleno(selectedRequest.getId());
+					scenePrehledZadosti(event);
+				}
 			} else if (selectedRequest.getSchvaleno() == "ano") {
 				ukazAlertGood("Žádost byla již schválena.");
 			} else if (selectedRequest.getSchvaleno() == "ne") {
